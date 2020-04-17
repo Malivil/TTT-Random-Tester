afcdaa6daccab40873a10a624305fdd4be28fd18
Malivil <mlebson@outlook.com>
2020-04-16 22:37:23 -0400

    Updated batch file

--- CreateAddonGMA.bat
+++ CreateAddonGMA.bat
@@ -1,2 +1,6 @@
 "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder %1
+echo Removing old file
+del TTT-Random-Tester.gma
+echo Renaming file
+ren %1.gma TTT-Random-Tester.gma
 pause
